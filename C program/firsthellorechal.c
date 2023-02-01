3254222ace837625b9f68f1bd49b0ea809056b6e
rechalgupta-D47 <122378461+rechalgupta-D47@users.noreply.github.com>
2023-02-01 23:02:26 +0530

    Delete C program directory

--- C program/firsthellorechal.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("Hello Rechal");
-	return 0;
-}
--- C program/name.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("Suhani,Apurva,Aakanksha,Kaumudi,Kavya");
-	printf("\nSuhani\nApurva\nAakanksha\nKaumudi\nKavya");
-	return 0;
-}
